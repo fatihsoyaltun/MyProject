24c19fe1091bcfb438bb7d5b7427fdc68d337d63
Fatih SOYALTUN <fatihsoyaltun@Yunus-Air.home>
2023-04-25 11:10:18 +0300

    owl btn upgrade

--- js/modeChoice.js
+++ js/modeChoice.js
@@ -5,7 +5,6 @@ const lightMode = document.querySelector(".lightMode");
 const darkMode = document.querySelector(".darkMode");
 const systemMode = document.querySelector(".systemMode");
 const items = document.querySelectorAll("body");
-const navDark = document.querySelector("nav-menu");
 
 // Determine user's system preference
 const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -31,11 +30,6 @@ darkMode.addEventListener("click", function () {
     darkMode.classList.add('modeActive');
     lightMode.classList.remove('modeActive');
     systemMode.classList.remove('modeActive');
-    // window.addEventListener('scroll', function() {
-    //     if (window.pageYOffset > 100 && navDark.classList.contains("scrolled")) {
-    //         navDark.classList.remove("scrolled");
-    //     }
-    // });
     //toggleDarkMode()
 });
 
